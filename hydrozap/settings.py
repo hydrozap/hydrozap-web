fe5bbb10c4fcb1ab53402389f50e12e00985b734
rainalert-ccsfp <rainalertccsfp@gmail.com>
2025-07-03 07:14:52 +0800

    Modified settings.py

--- hydrozap/settings.py
+++ hydrozap/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = [
     "192.168.100.77",  # Add your local network IP here
     "192.168.93.88",
     "192.168.100.15",
-    "hydrozap-backend.onrender.com",
+    "hydrozap-web.onrender.com",
     "hydrozap.vercel.app",
     
 ]
